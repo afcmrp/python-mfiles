43983661d3229036c84e22f8c3826e11bb033e14
Emil Hjelm <emil.hjelm@climeon.com>
2020-04-04 12:37:47 +0200

    Refer to documentation

--- setup.py
+++ setup.py
@@ -3,17 +3,7 @@ from setuptools import setup, find_packages
 PYPI_DESCRIPTION = """
 Python wrapper around the M-Files API.
 
-Enables search, upload, download and creation of objects in M-Files vaults.
-When authentication is needed credentials are fetched from environment
-variables ``MFILES_USER`` and ``MFILES_PASS``. If they are not set the
-credentials are fetched from user input using ``input()`` and ``get_pass()``.
-To supply credentials programatically you can call the ``login()`` method with
-username and password before using the API, and all subsequent calls will be
-authenticated with the same token.
-
-M-Files property IDs for all object types are abstracted, so you can upload a
-``Document`` using ``upload_file()`` with ``object_type="Document"`` and
-correct IDs will be fetched from the server.
+Documentation: https://mfiles.readthedocs.io/en/latest/
 """
 
 setup(

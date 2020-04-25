5f42db699bb9ae4bc02f9f0a16aba6360e448a6a
Emil Hjelm <emil.hjelm@climeon.com>
2020-04-25 23:58:25 +0200

    Added tbump to simplify releases

--- setup.py
+++ setup.py
@@ -11,14 +11,13 @@ Documentation: https://mfiles.readthedocs.io/en/latest/
 setup(
     name='mfiles',
     packages=find_packages(),
-    version='0.3',
+    version='0.3.0',
     license='MIT',
     description='M-Files API wrapper',
     long_description=PYPI_DESCRIPTION,
     author='Emil Hjelm',
     author_email='emil.hjelm@climeon.com',
     url='https://github.com/afcmrp/mfiles',
-    download_url='https://github.com/user/reponame/archive/v_03.tar.gz',
     keywords=['M-Files', 'mfiles', 'REST', 'API'],
     install_requires=[
         'requests',

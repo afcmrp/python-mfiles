282e07e41adb3753fd1fc685479ac9024f80c65c
Emil Hjelm <emil.hjelm@climeon.com>
2020-04-29 12:38:45 +0200

    Omit vault id for test to avoid trying to login

--- test/client_test.py
+++ test/client_test.py
@@ -4,8 +4,7 @@ from mfiles.client import MFilesClient
 
 TEST_USER = "test_user"
 TEST_PASS = "test_pass"
-TEST_VAULT = "test_vault"
 
 def test_init():
     """Test creation of MFilesClient object."""
-    MFilesClient(user=TEST_USER, password=TEST_PASS, vault=TEST_VAULT)
+    MFilesClient(user=TEST_USER, password=TEST_PASS)

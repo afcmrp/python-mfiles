c37720b4fc8eadd8f2f81b639acf0383ddbae0f0
Emil Hjelm <emil.hjelm@climeon.com>
2020-04-29 12:32:38 +0200

    Added test credentials

--- test/client_test.py
+++ test/client_test.py
@@ -2,6 +2,10 @@
 
 from mfiles.client import MFilesClient
 
+TEST_USER = "test_user"
+TEST_PASS = "test_pass"
+TEST_VAULT = "test_vault"
+
 def test_init():
     """Test creation of MFilesClient object."""
-    MFilesClient()
+    MFilesClient(user=TEST_USER, password=TEST_PASS, vault=TEST_VAULT)
